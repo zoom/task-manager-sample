cdf2fabffda2de12d709593664d2186752bfa6f9
Donte Small <donte.small@zoom.us>
2025-04-04 19:47:42 -0400

    Updated deep linking support

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,5 +1,6 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
+
 import {getDeeplink} from "@/app/lib/zoom-api";
 import {redirect} from "next/navigation";
 import {NextResponse} from "next/server";
