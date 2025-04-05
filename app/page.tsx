b331e3abd3f25cb88bb27fc5674c21c7b85709b9
Donte Small <donte.small@zoom.us>
2025-04-05 06:06:32 -0400

    Removed Zoom App Integreation Steps

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,5 @@
 import Hero from '@/components/hero'
-import {headers} from "next/headers";
-import dynamic from "next/dynamic";
 
-import SignInWithZoomButton from '@/components/taskmanger/zoom-app-signin-btn';
-
-
-export default async function Page() {
-    let Zoom;
-
-    const headersList = await headers();
-    const isZoom = headersList.has('x-zoom-app-device-type');
-
-    const loadZoomApp = () => {
-        if (!isZoom) return  <SignInWithZoomButton/>;
-
-        Zoom = dynamic(() => import('@/components/zoom-app'));
-
-        return (<Zoom/>)
-    }
-
-    return (
-        <div>
-            <Hero></Hero>
-            {loadZoomApp()}
-           
-        </div>
-    )
+export default function Page() {
+    return (<Hero></Hero>)
 }
