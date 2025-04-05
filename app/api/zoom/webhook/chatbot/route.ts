d063338634ed040b61c98c859837a6c6135e3ea9
Donte Small <donte.small@zoom.us>
2025-04-05 05:41:18 -0400

    Add chatbot and teamchat webhook handler for Zoom events

---
+++ app/api/zoom/webhook/chatbot/route.ts
@@ -0,0 +1,62 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import crypto from "crypto";
+
+
+const ZOOM_SECRET_TOKEN = process.env.ZOOM_SECRET_TOKEN || "";
+const ZOOM_VERIFICATION_TOKEN = process.env.ZOOM_VERIFICATION_TOKEN || "";
+
+
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+
+    if (!body) {
+      return NextResponse.json({ error: "No Request Body" }, { status: 400 });
+    }
+
+    // Handle Zoom Webhook URL validation
+    if (body.event === "endpoint.url_validation") {
+      const hashForValidation = crypto
+        .createHmac("sha256", ZOOM_SECRET_TOKEN)
+        .update(body.payload.plainToken)
+        .digest("hex");
+
+      return NextResponse.json({
+        plainToken: body.payload.plainToken,
+        encryptedToken: hashForValidation,
+      });
+    }
+
+    // // Verify Authorization Header (Optional but Recommended)
+    const authHeader = req.headers.get("authorization") || "";
+    if (authHeader !== ZOOM_VERIFICATION_TOKEN) {
+      return NextResponse.json({ error: "Unauthorized request" }, { status: 401 });
+    }
+
+    // Process Webhook Events
+    if (body.event === "bot_notification") {
+      console.log(" 🏁 Bot Notification Received", body.payload);
+
+    }
+
+    // Process Webhook Events
+    switch (body.event) {
+      case 'bot_installed':
+        console.log("✅ Bot Installed", body.payload);
+        return NextResponse.json({ message: "Bot installed successfully." }, { status: 200 });
+
+      case 'team_chat.link_shared':
+        console.log("🤖 Unfurlable Link Shared", body.payload);
+        return NextResponse.json({ message: "Bot installed successfully." }, { status: 200 });
+
+      default:
+        console.warn("⚠️ Unknown event:", body.event);
+        return NextResponse.json({ error: "Unknown event type" }, { status: 400 });
+    }
+  } catch (error) {
+    console.error("❌ Error handling Zoom webhook:", error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
