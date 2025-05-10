9405948d37c17c45fe29475196bc1355f30889de
Donte Small <donte.small@zoom.us>
2025-05-10 10:07:44 -0400

    Starter codebase

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,6 +5,8 @@ import ProjectsContainer from "@/components/projects/projects-container";
 export default async function DashboardPage() {
     const supabase = await createClient();
 
+    
+
 
     return (
         <div className="flex flex-row w-full max-w-full">
