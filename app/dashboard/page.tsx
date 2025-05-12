4be9b6f7b0c3c4bc64d1c5be8d5ea67f39a2f624
Donte Small <donte.small@zoom.us>
2025-05-12 12:17:01 -0400

    Starting point for step 00

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,17 @@
-import {createClient} from "@/utils/supabase/server";
-import {redirect} from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 import ProjectsContainer from "@/components/projects/projects-container";
 
 export default async function DashboardPage() {
     const supabase = await createClient();
 
-    
+
+
 
 
     return (
         <div className="flex flex-row w-full max-w-full">
-            <ProjectsContainer/>
+            <ProjectsContainer />
         </div>
     );
 }
