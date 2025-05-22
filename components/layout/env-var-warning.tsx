b1fc6e7a4afb84dc2929067a7156932f78c99fc2
Donte Small <donte.small@zoom.us>
2025-05-22 02:54:40 -0400

    Added lib Zoom Helpers

--- components/layout/env-var-warning.tsx
+++ components/layout/env-var-warning.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import { Badge } from "./ui/badge";
-import { Button } from "./ui/button";
+import { Badge } from "../ui/badge";
+import { Button } from "../ui/button";
 
 export function EnvVarWarning() {
   return (
