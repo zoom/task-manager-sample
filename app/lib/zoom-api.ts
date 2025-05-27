205bd0153b931fed770af5c7cad2b50acbe6951e
Donte Small <donte.small@zoom.us>
2025-05-27 14:19:04 -0400

    Beta Golden Version

--- app/lib/zoom-api.ts
+++ app/lib/zoom-api.ts
@@ -167,13 +167,6 @@ export async function getDeeplink(
     // Zoom injects the act field into the decrypted x-zoom-app-context when the Zoom App is opened via the deeplink.
     const body = { action: data.action }; 
 
-    // If the action character limit is exceeded, Zoom client will freeze.
-    // if (body.action.length > 256) {
-    //   console.warn("⚠️  Action exceeds 256 characters, truncating...", '\n');
-    //   console.log("🚨 Possible bug with Zoom client freezing if action exceeds 256 characters and the act field is not truncated.", '\n');
-    //   body.action = body.action.substring(0, 256); // Possible bug with Zoom client freezing if action exceeds 256 characters and the act field is not truncated.
-    // }
-
     const response = await apiRequest("POST", "/zoomapp/deeplink", token, body);
     
     console.log("✅ Zoom Deeplink API response:", response);
